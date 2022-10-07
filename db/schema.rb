7dcea25c1ffa6d75fd25066b0ee8bd34f227f87b
ilham bouaik <ibouaik@gmail.com>
2022-10-07 15:46:18 +0100

    create users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_07_144137) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_07_144433) do
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
