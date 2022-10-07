1a15e4f65309f64c3c7598c1d357e97c17d65f96
ilham bouaik <ibouaik@gmail.com>
2022-10-07 15:42:47 +0100

    drop users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_07_092325) do
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2022_10_07_144137) do
 end
